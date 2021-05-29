ac8b8b7f35676a3d47610e8619a06662c2d1b7da
Karl Frick <karl.frick5@gmail.com>
2021-05-29 17:52:32 -0400

    More CSS

--- src/App.jsx
+++ src/App.jsx
@@ -18,29 +18,32 @@ export class App extends Component {
   componentDidMount() {
     this.fetchData()
   }
+
   render() {
+    const movieResults = this.state.results.map(results => {
+      return (
+        <article key={results.id}>
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w185/${results.poster_path}`}
+              alt="Poster image of the movie"
+            ></img>
+          </div>
+          <ul>
+            <li>{results.title}</li>
+            <li>Release date: {results.release_date}</li>
+            <li>{results.overview}</li>
+          </ul>
+        </article>
+      )
+    })
     return (
       <main>
         <header>
           <h1>Party like it's 1989!</h1>
+          <h2>Popular movies from 1989</h2>
         </header>
-        <div>
-          <ul>
-            {this.state.results.map((results, id, poster_path) => {
-              return (
-                <li key={results.id}>
-                  {results.original_title}:
-                  <p>
-                    <img
-                      src={`https://image.tmdb.org/t/p/w185/${results.poster_path}`}
-                    ></img>
-                  </p>
-                  <p>{results.overview}</p>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
+        <section>{movieResults}</section>
       </main>
     )
   }
